d75330f68db58dd60be0a3bd51fee439cab46a1a
Nitthiwat <nitthiwat.j@kkumail.com>
2022-03-24 00:39:42 +0700

    saveorder

--- cart.php
+++ cart.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
+
 $Product_id = $_GET['Product_id'];
 $act = $_GET['act'];
 
 if ($act == 'add' && !empty($Product_id)) {
     if (isset($_SESSION['cart'][$Product_id])) {
-        $_SESSION['cart'][$Product_id]++;
+        $_SESSION['cart'][$Product_id];
     } else {
         $_SESSION['cart'][$Product_id] = 1;
     }
@@ -24,72 +26,64 @@ if ($act == 'update') {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Shopping Cart</title>
 </head>
 
 <body>
-    <?php include('usermenu.php'); ?>
-
-    <div class="container my-5">
-        <div class="well">
-            <span class="my-5 " style="font-size:25px; color:blue">
-                <center><strong>ตะกร้าสินค้า</strong></center>
-            </span>
-            <!-- <a href="add.php">Add product</a> -->
-            <table class="table table-striped table-bordered table-hover">
-                <thead>
-                    <th>สินค้า</th>
-                    <th>ชื่อสินค้า</th>
-                    <th>ที่อยู่การจัดส่ง</th>
-                    <th>ราคาสินค้า</th>
-                    <th>จำนวน</th>
-                    <th>ราคารวม</th>
-                </thead>
-                <tbody>
-                    <?php
-                    $total = 0;
-                    if (!empty($_SESSION['cart'])) {
-                        include("conn.php");
-                        foreach ($_SESSION['cart'] as $Product_id => $qty) {
-                            $sql = "select * from product where Product_id='$Product_id'";
-                            $query = mysqli_query($conn, $sql);
-                            $row = mysqli_fetch_array($query);
-                            $sum = $row['Product_price'] * $qty;
-                            $total += $sum;
-                            echo "<tr>";
-                            echo "<td width='334'>" . $row["Product_name"] . "</td>";
-                            echo "<td width='46' align='right'>" . number_format($row["Product_price"], 2) . "</td>";
-                            echo "<td width='57' align='right'>";
-                            echo "<input type='text' name='amount[$Product_id]' value='$qty' size='2'/></td>";
-                            echo "<td width='93' align='right'>" . number_format($sum, 2) . "</td>";
-                            //remove product
-                            echo "<td width='46' align='center'><a href='cart.php?Product_id=$Product_id&act=remove'>ลบ</a></td>";
-                            echo "</tr>";
-                        }
-                        echo "<tr>";
-                        echo "<td colspan='3' bgcolor='#CEE7FF' align='center'><b>ราคารวม</b></td>";
-                        echo "<td align='right' bgcolor='#CEE7FF'>" . "<b>" . number_format($total, 2) . "</b>" . "</td>";
-                        echo "<td align='left' bgcolor='#CEE7FF'></td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                    <tr>
-                        <td><a href="product.php">กลับหน้ารายการสินค้า</a></td>
-                        <td colspan="4" align="right">
-                            <input type="submit" name="button" id="button" value="ปรับปรุง" />
-                            <input type="button" name="Submit2" value="สั่งซื้อ" onclick="window.location='confirm.php';" />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <form id="frmcart" name="frmcart" method="post" action="?Product_id=<?php echo $Product_id ?>&act=update">
+        <table width="600" border="0" align="center" class="square">
+            <tr>
+                <td colspan="5" bgcolor="#CCCCCC">
+                    <b>ตะกร้าสินค้า</span>
+                </td>
+            </tr>
+            <tr>
+                <td bgcolor="#EAEAEA">สินค้า</td>
+                <td align="center" bgcolor="#EAEAEA">ราคา</td>
+                <td align="center" bgcolor="#EAEAEA">จำนวน</td>
+                <td align="center" bgcolor="#EAEAEA">รวม(บาท)</td>
+                <td align="center" bgcolor="#EAEAEA">ลบ</td>
+            </tr>
+            <?php
+            $total = 0;
+            if (!empty($_SESSION['cart'])) {
+                include("conn.php");
+                foreach ($_SESSION['cart'] as $Product_id => $qty) {
+                    $sql = "select * from product where Product_id='$Product_id'";
+                    $query = mysqli_query($conn, $sql);
+                    $row = mysqli_fetch_array($query);
+                    $sum = $row['Product_price'] * $qty;
+                    $total += $sum;
+                    echo "<tr>";
+                    echo "<td width='334'>" . $row["Product_name"] . "</td>";
+                    echo "<td width='46' align='right'>" . number_format($row["Product_price"], 2) . "</td>";
+                    echo "<td width='57' align='right'>";
+                    echo "<input type='text' name='amount[$Product_id]' value='$qty' size='2'/></td>";
+                    echo "<td width='93' align='right'>" . number_format($sum, 2) . "</td>";
+                    //remove product
+                    echo "<td width='46' align='center'><a href='cart.php?Product_id=$Product_id&act=remove'>ลบ</a></td>";
+                    echo "</tr>";
+                }
+                echo "<tr>";
+                echo "<td colspan='3' bgcolor='#CEE7FF' align='center'><b>ราคารวม</b></td>";
+                echo "<td align='right' bgcolor='#CEE7FF'>" . "<b>" . number_format($total, 2) . "</b>" . "</td>";
+                echo "<td align='left' bgcolor='#CEE7FF'></td>";
+                echo "</tr>";
+            }
+            ?>
+            <tr>
+                <td><a href="product_test.php">กลับหน้ารายการสินค้า</a></td>
+                <td colspan="4" align="right">
+                    <input type="submit" name="button" id="button" value="ปรับปรุง" />
+                    <input type="button" name="Submit2" value="สั่งซื้อ" onclick="window.location='confirm.php';" />
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
 
 </html>
