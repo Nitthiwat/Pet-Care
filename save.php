a6742b5d601b04370de0f0e1a7a74910dddfa12b
Nitthiwat <nitthiwat.j@kkumail.com>
2022-03-23 23:38:07 +0700

    save

--- save.php
+++ save.php
@@ -5,13 +5,29 @@
     $Product_price = $_POST["Product_price"];
     $Product_detail = $_POST["Product_detail"];
     $Product_qty = $_POST["Product_Qty"];
-    $filename = $_FILES["Product_img"]["name"];
-    $filTmpname = $_FILES["Product_img"]["tmp_name"];
-    $filelocation = 'img/' . $filename;
-    move_uploaded_file($filTmpname, $filelocation.$filename);
-    $pdfilelocation = $filelocation;
     $pd = $_POST["PType_id"];
     $pt = $_POST["PetType_id"];
+    echo $pt;
+
+    $file = $_FILES['Product_img'];
+    $filename = $_FILES["Product_img"]["name"];
+    $filTmpname = $_FILES["Product_img"]["tmp_name"];
+    $fileExt = explode(".", $filename);
+    $fileAcExt = strtolower(end($fileExt));
+    $newFilename = time() . "." . $fileAcExt;
+    if($pt == "PT001"){
+        $filelocation = 'img/ShopDog/' . $filename;
+    }else if($pt == "PT002"){
+        $filelocation = 'img/ShopCat/' . $filename;
+    }else if($pt == "PT003"){
+        $filelocation = 'img/ShopFish/' . $filename;
+    }else if($pt == "PT004"){
+        $filelocation = 'img/ShopBird/' . $filename;
+    }
+    move_uploaded_file($filTmpname, $filelocation);
+    $pdfilelocation = $filelocation;
+
+
     $pd_query = mysqli_query($conn,"SELECT * FROM producttype WHERE PType_id='$pd'");
     while ($pd_row = mysqli_fetch_array($pd_query))
     $pd_type = $pd_row['PType_id'];
